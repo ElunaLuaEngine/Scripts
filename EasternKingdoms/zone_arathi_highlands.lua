e29e6ed2602b0c8211dab5fa8e20501d51cb910f
Easelm <easelm@live.com>
2013-11-06 15:50:05 -0500

    UPDATED: zone_arathi_highlands.lua
    Corrected spacing

---
+++ EasternKingdoms/zone_arathi_highlands.lua
@@ -0,0 +1,98 @@
+local Waypoints =
+{
+    { 1, 1, -2073.519, -2123.502, 18.433033 },
+    { 4, 5, -2073.519, -2123.502, 18.433033 },
+    { 5, 6, -2074.780, -2089.530, 8.972266 },
+    { 6, 7, -2074.940, -2089.247, 8.911692 },
+    { 8, 8, -2066.248, -2086.332, 9.009366 }
+}
+
+local escortPlayer = nil
+local currentWP = 0
+
+function OnQuestAccept(event, player, creature, quest)
+    if (quest:GetId() == 665) then
+        escortPlayer = player
+        currentWP = 0
+        creature:SendCreatureTalk(0, player:GetGUID())
+        creature:SetWalk(true)
+        creature:MoveTo(0, -2077.985, -2093.242, 10.001955)
+        creature:SetFaction(35)
+    end
+end
+
+function OnEnterCombat(event, creature, target)
+    creature:SendCreatureTalk(4, 0)
+end
+
+function OnReachWP(event, creature, pointType, waypointId)
+    currentWP = waypointId + 1
+    local delay = 0
+
+    if (currentWP == 1) then
+        delay = 1000
+    elseif (currentWP == 2) then
+        creature:SendCreatureTalk(1, escortPlayer:GetGUID())
+        creature:RegisterEvent(OnMoveForward, 3500, 1)
+    elseif (currentWP == 3) then
+        creature:SendCreatureTalk(3, 0)
+        creature:RegisterEvent(OnSummon, 8000, 1)
+        creature:RegisterEvent(OnAlmostDone, 15000, 1)
+        creature:RegisterEvent(OnFinish, 22000, 1)
+    elseif (currentWP == 4) then
+        delay = 1000
+        creature:SendCreatureTalk(7, escortPlayer:GetGUID())
+    elseif (currentWP == 6) then
+        delay = 2000
+    elseif (currentWP == 8) then
+        delay = 1000
+    elseif (currentWP == 9) then
+        creature:RemoveEvents()
+        creature:SendCreatureTalk(8, 0)
+        creature:SendCreatureTalk(9, escortPlayer:GetGUID())
+        escortPlayer:GroupEventHappens(665, creature)
+        creature:Despawn(3000)
+        escortPlayer = nil
+        currentWP = 0
+    end
+    if (delay > 0) then
+        creature:RegisterEvent(OnMove, delay, 1)
+    end
+end
+
+function OnJustSummoned(event, creature, summoned)
+    summoned:AttackStart(escortPlayer)
+end
+
+function OnMove(event, delay, pCall, creature)
+    for k,_ in ipairs(Waypoints) do
+        if (Waypoints[k][1] == currentWP) then
+            creature:MoveTo(Waypoints[k][2], Waypoints[k][3], Waypoints[k][4], Waypoints[k][5])
+        end
+    end
+end
+
+function OnMoveForward(event, delay, pCall, creature)
+    creature:SendCreatureTalk(2, escortPlayer:GetGUID())
+    creature:MoveTo(currentWP, -2043.243, -2154.018, 20.232119)
+end
+
+function OnSummon(event, delay, pCall, creature)
+    creature:SpawnCreature(2776, -2052.96, -2142.49, 20.15, 1.0, 5)
+    creature:SpawnCreature(2776, -2052.96, -2142.49, 20.15, 1.0, 5)
+end
+
+function OnAlmostDone(event, delay, pCall, creature)
+    creature:SendCreatureTalk(5, escortPlayer:GetGUID())
+end
+
+function OnFinish(event, delay, pCall, creature)
+    creature:SendCreatureTalk(6, escortPlayer:GetGUID())
+    creature:SetWalk(false)
+    creature:MoveTo(currentWP, -2070.117, -2126.960, 19.514397)
+end
+
+RegisterCreatureEvent(2768, 1, OnEnterCombat)
+RegisterCreatureEvent(2768, 6, OnReachWP)
+RegisterCreatureEvent(2768, 19, OnJustSummoned)
+RegisterCreatureEvent(2768, 31, OnQuestAccept)
