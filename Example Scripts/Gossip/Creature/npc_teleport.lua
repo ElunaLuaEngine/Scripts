21c45a3f2cd685f882db5109f96767d6125b144f
Foereaper <Foereaper@users.noreply.github.com>
2013-12-28 07:14:00 +0100

    Update npc_teleport.lua
    Added new version of the tabled teleporter, including some info.

--- Example Scripts/Gossip/Creature/npc_teleport.lua
+++ Example Scripts/Gossip/Creature/npc_teleport.lua
@@ -1,111 +1,109 @@
-local NPC_ID = NPCID
+--[[	How to add new locations!
+		
+		Example:
+		
+		The first line will be the main menu ID (Here [1], 
+		increment this for each main menu option!),
+		the main menu gossip title (Here "Horde Cities"),
+		as well as which faction can use the said menu (Here 1 (Horde)). 
+		0 = Alliance, 1 = Horde, 2 = Both
+		
+		The second line is the name of the main menu's sub menus, 
+		separated by name (Here "Orgrimmar") and teleport coordinates
+		using Map, X, Y, Z, O (Here 1, 1503, -4415.5, 22, 0)
+		
+		[1] = { "Horde Cities", 1,	--  
+			{"Orgrimmar", 1, 1503, -4415.5, 22, 0},
+		},
+		
+		You can copy paste the above into the script and change the values as informed.
+]]
 
-local T = 
-	{
-		-- Horde Cities
-	
-		{1, 1503, -4415.5, 22, 0},		-- Orgrimmar		intid 1
-		{0, 1831, 238.5, 61.6, 0},		-- Undercity		intid 2
-		{1, -1278, 122, 132, 0},		-- Thunderbluff		intid 3
-		{530, 9484, -7294, 15, 0},		-- Silvermoon		intid 4
 
-		-- Ally Cities
+local UnitEntry = 1
 
-		{0, -8905, 560, 94, 0.62},		-- Stormwind		intid 5
-		{0, -4795, -1117, 499, 0},		-- Ironforge		intid 6
-		{1, 9952, 2280.5, 1342, 1.6},		-- Darnassus		intid 7
-		{530, -3863, -11736, -106, 2},		-- The Exodar		intid 8
-		
-		-- Outlands Locations
-		
-		{530, 1481, 6829, 107, 6},		-- Blade's Edge		intid 9
-		{530, -249, 947, 85, 2},		-- Hellfire		intid 10
-		{530, -1769, 7150, -9, 2},		-- Nagrand		intid 11
-		{530, 3043, 3645, 143, 2},		-- Netherstorm		intid 12
-		{530, -3034, 2937, 87, 5},		-- Shadowmoon		intid 13
-		{530, -1942, 4689, -2, 5},		-- Terokkar		intid 14
-		{530, -217, 5488, 23, 2},		-- Zangarmarsh		intid 15
-		{530, -1822, 5417, 1, 3},		-- Shattrath		intid 16
-		
-		-- Northrend Locations
-		
-		{571, 3230, 5279, 47, 3},		-- Borean Tundra	intid 17
-		{571, 5732, 1016, 175, 3.6},		-- Crystalsong		intid 18
-		{571, 3547, 274, 46, 1.6},		-- Dragonblight		intid 19
-		{571, 3759, -2672, 177, 3},		-- Grizzly Hills	intid 20
-		{571, 772, -2905, 7, 5},		-- Howling Fjord	intid 21
-		{571, 8517, 676, 559, 4.7},		-- Icecrown		intid 22
-		{571, 5571, 5739, -75, 2},		-- Sholazar		intid 23
-		{571, 6121, -1025, 409, 4.7},		-- Storm Peaks		intid 24
-		{571, 5135, 2840, 408, 3},		-- Wintergrasp		intid 25
-		{571, 5761, -3547, 387, 5},		-- Zul'Drak		intid 26
-		{571, 5826, 470, 659, 1.4},		-- Dalaran		intid 27
-		
-		-- PvP Locations
-		
-		{0, -13229, 226, 33, 1},		-- Gurubashi		intid 28
-		{1, -3669, 1094, 160, 3},		-- Dire Maul		intid 29
-		{530, -1983, 6562, 12, 2},		-- Nagrand		intid 30
-		{530, 2910, 5976, 2, 4},		-- Blade's Edge		intid 31
-	}
+local T = {
+	[1] = { "Horde Cities", 1, -- This will be the main menu title, as well as which faction can use the said menu. 0 = Alliance, 1 = Horde, 2 = Both
+		{"Orgrimmar", 1, 1503, -4415.5, 22, 0},
+		{"Undercity", 0, 1831, 238.5, 61.6, 0},
+		{"Thunderbluff", 1, -1278, 122, 132, 0},
+		{"Silvermoon", 530, 9484, -7294, 15, 0},
+	},
+	[2] = { "Alliance Cities", 0,
+		{"Stormwind", 0, -8905, 560, 94, 0.62},
+		{"Ironforge", 0, -4795, -1117, 499, 0},
+		{"Darnassus", 1, 9952, 2280.5, 1342, 1.6},
+		{"The Exodar", 530, -3863, -11736, -106, 2},
+	},
+	[3] = { "Outlands Locations", 2,
+		{"Blade's Edge Mountains", 530, 1481, 6829, 107, 6},
+		{"Hellfire Peninsula", 530, -249, 947, 85, 2},
+		{"Nagrand", 530, -1769, 7150, -9, 2},
+		{"Netherstorm", 530, 3043, 3645, 143, 2},
+		{"Shadowmoon Valley", 530, -3034, 2937, 87, 5},
+		{"Terokkar Forest", 530, -1942, 4689, -2, 5},
+		{"Zangarmarsh", 530, -217, 5488, 23, 2},
+		{"Shattrath", 530, -1822, 5417, 1, 3},
+	},
+	[4] = { "Northrend Locations", 2,
+		{"Borean Tundra", 571, 3230, 5279, 47, 3},
+		{"Crystalsong Forest", 571, 5732, 1016, 175, 3.6},
+		{"Dragonblight", 571, 3547, 274, 46, 1.6},
+		{"Grizzly Hills", 571, 3759, -2672, 177, 3},
+		{"Howling Fjord", 571, 772, -2905, 7, 5},
+		{"Icecrown Glaicer", 571, 8517, 676, 559, 4.7},
+		{"Sholazar Basin", 571, 5571, 5739, -75, 2},
+		{"Storm Peaks", 571, 6121, -1025, 409, 4.7},
+		{"Wintergrasp", 571, 5135, 2840, 408, 3},
+		{"Zul'Drak", 571, 5761, -3547, 387, 5},
+		{"Dalaran", 571, 5826, 470, 659, 1.4},
+	},
+	[5] = { "PvP Locations", 2,
+		{"Gurubashi Arena", 0, -13229, 226, 33, 1},
+		{"Dire Maul Arena", 1, -3669, 1094, 160, 3},
+		{"Nagrand Arena", 530, -1983, 6562, 12, 2},
+		{"Blade's Edge Arena", 530, 2910, 5976, 2, 4},
+	},
+};
 
-function Teleporter_Gossip(event, plr, unit)
-	plr:GossipMenuAddItem(0, "Main Cities", 0, 1001, 0)
-	plr:GossipMenuAddItem(0, "Outlands Locations", 0, 1002, 0)
-	plr:GossipMenuAddItem(0, "Northrend Locations", 0, 1003, 0)
-	plr:GossipMenuAddItem(0, "PvP Locations", 0, 1004, 0)
-	plr:GossipSendMenu(1, unit)
-end
+--[[ CODE STUFFS! DO NOT EDIT BELOW ]]--
+--[[ UNLESS YOU KNOW WHAT YOU'RE DOING! ]]--
 
-function Teleporter_Event(event, plr, unit, arg2, intid)
-	if(intid >= 1) and (intid <= 100) then
-		plr:Teleport(T[intid][1], T[intid][2], T[intid][3], T[intid][4], T[intid][5])
-	elseif(intid == 1000) then
-		Teleporter_Gossip(event, plr, unit)
-	elseif(intid >= 1001) and (intid <= 1004) then
-        if(intid == 1001) then
-			if(plr:GetTeam() == 1) then
-				plr:GossipMenuAddItem(2, "Orgrimmar", 0, 1)
-				plr:GossipMenuAddItem(2, "Undercity", 0, 2)
-				plr:GossipMenuAddItem(2, "Thunderbluff", 0, 3)
-				plr:GossipMenuAddItem(2, "Silvermoon", 0, 4)
-			else
-				plr:GossipMenuAddItem(2, "Stormwind", 0, 5)
-				plr:GossipMenuAddItem(2, "Ironforge", 0, 6)
-				plr:GossipMenuAddItem(2, "Darnassus", 0, 7)
-				plr:GossipMenuAddItem(2, "The Exodar", 0, 8)
+function Teleporter_Gossip(event, player, unit)
+	if (#T <= 10) then
+		for i, v in ipairs(T) do
+			if(v[2] == 2 or v[2] == player:GetTeam()) then
+				player:GossipMenuAddItem(0, v[1], 0, i)
 			end
-		elseif(intid == 1002) then
-			plr:GossipMenuAddItem(2, "Blade's Edge Mountains", 0, 9)
-			plr:GossipMenuAddItem(2, "Hellfire Peninsula", 0, 10)
-			plr:GossipMenuAddItem(2, "Nagrand", 0, 11)
-			plr:GossipMenuAddItem(2, "Netherstorm", 0, 12)
-			plr:GossipMenuAddItem(2, "Shadowmoon Valley", 0, 13)
-			plr:GossipMenuAddItem(2, "Terokkar Forest", 0, 14)
-			plr:GossipMenuAddItem(2, "Zangramarsh", 0, 15)
-			plr:GossipMenuAddItem(2, "Shattrath", 0, 16)
-		elseif(intid == 1003) then
-			plr:GossipMenuAddItem(2, "Borean Tundra", 0, 17)
-			plr:GossipMenuAddItem(2, "Crystalsong Forest", 0, 18)
-			plr:GossipMenuAddItem(2, "Dragonblight", 0, 19)
-			plr:GossipMenuAddItem(2, "Grizzly Hills", 0, 20)
-			plr:GossipMenuAddItem(2, "Howling Fjords", 0, 21)
-			plr:GossipMenuAddItem(2, "Icecrown Glaicer", 0, 22)
-			plr:GossipMenuAddItem(2, "Sholazar Basin", 0, 23)
-			plr:GossipMenuAddItem(2, "Storm Peaks", 0, 24)
-			plr:GossipMenuAddItem(2, "Wintergrasp", 0, 25)
-			plr:GossipMenuAddItem(2, "Zul'Drak", 0, 26)
-			plr:GossipMenuAddItem(2, "Dalaran", 0, 27)
-		elseif(intid == 1004) then
-			plr:GossipMenuAddItem(9, "Gurubashi Arena", 0, 28)
-			plr:GossipMenuAddItem(9, "Dire Maul Arena", 0, 29)
-			plr:GossipMenuAddItem(9, "Nagrand Arena", 0, 30)
-			plr:GossipMenuAddItem(9, "Blade's Edge Arena", 0, 31)
 		end
-		plr:GossipMenuAddItem(0, "Back", 0, 1000)
-		plr:GossipSendMenu(1, unit)
+		player:GossipSendMenu(1, unit)
+	else
+		print("This teleporter only supports 10 different menus.")
+	end
+end	
+
+function Teleporter_Event(event, player, unit, sender, intid, code)
+	if(intid == 0) then
+		Teleporter_Gossip(event, player, unit)
+	elseif(intid <= 10) then
+		for i, v in ipairs(T[intid]) do
+			if (i > 2) then
+				player:GossipMenuAddItem(0, v[1], 0, intid..i)
+			end
+		end
+		player:GossipMenuAddItem(0, "Back", 0, 0)
+		player:GossipSendMenu(1, unit)
+	elseif(intid > 10) then
+		for i = 1, #T do
+			for j, v in ipairs(T[i]) do
+				if(intid == tonumber(i..j)) then
+					player:GossipComplete()
+					player:Teleport(v[2], v[3], v[4], v[5], v[6])
+				end
+			end
+		end
 	end
 end
 
-RegisterCreatureGossipEvent(NPC_ID, 1, Teleporter_Gossip)
-RegisterCreatureGossipEvent(NPC_ID, 2, Teleporter_Event)
+RegisterCreatureGossipEvent(UnitEntry, 1, Teleporter_Gossip)
+RegisterCreatureGossipEvent(UnitEntry, 2, Teleporter_Event)
