7aed0857cef9c7d80675a413478343a9fc5247d5
Rochet2 <rochet2@post.com>
2014-06-27 18:50:49 +0300

    Update Dynamic teleporter.lua
    Removed unnecessary loops and logic by using sender.
    This also removed the limit of 10 options.
    Simplified some of the code by removing unnecessary checks.

--- Custom/Dynamic teleporter.lua
+++ Custom/Dynamic teleporter.lua
@@ -1,24 +1,24 @@
---[[	How to add new locations!
-		
-		Example:
-		
-		The first line will be the main menu ID (Here [1], 
-		increment this for each main menu option!),
-		the main menu gossip title (Here "Horde Cities"),
-		as well as which faction can use the said menu (Here 1 (Horde)). 
-		0 = Alliance, 1 = Horde, 2 = Both
-		
-		The second line is the name of the main menu's sub menus, 
-		separated by name (Here "Orgrimmar") and teleport coordinates
-		using Map, X, Y, Z, O (Here 1, 1503, -4415.5, 22, 0)
-		
-		[1] = { "Horde Cities", 1,	--  
-			{"Orgrimmar", 1, 1503, -4415.5, 22, 0},
-		},
-		
-		You can copy paste the above into the script and change the values as informed.
-]]
+--[==[
+    = How to add new locations =
 
+    Example:
+
+    The first line will be the main menu ID (Here [1], 
+    increment this for each main menu option!),
+    the main menu gossip title (Here "Horde Cities"),
+    as well as which faction can use the said menu (Here 1 (Horde)). 
+    0 = Alliance, 1 = Horde, 2 = Both
+
+    The second line is the name of the main menu's sub menus, 
+    separated by name (Here "Orgrimmar") and teleport coordinates
+    using Map, X, Y, Z, O (Here 1, 1503, -4415.5, 22, 0)
+
+    [1] = { "Horde Cities", 1,	--  
+        {"Orgrimmar", 1, 1503, -4415.5, 22, 0},
+    },
+
+    You can copy paste the above into the script and change the values as informed.
+]==]
 
 local UnitEntry = 1
 
@@ -64,46 +64,46 @@ local T = {
 		{"Nagrand Arena", 530, -1983, 6562, 12, 2},
 		{"Blade's Edge Arena", 530, 2910, 5976, 2, 4},
 	},
-};
+}
 
---[[ CODE STUFFS! DO NOT EDIT BELOW ]]--
---[[ UNLESS YOU KNOW WHAT YOU'RE DOING! ]]--
+-- CODE STUFFS! DO NOT EDIT BELOW
+-- UNLESS YOU KNOW WHAT YOU'RE DOING!
 
-local function Teleporter_Gossip(event, player, unit)
-	if (#T <= 10) then
-		for i, v in ipairs(T) do
-			if(v[2] == 2 or v[2] == player:GetTeam()) then
-				player:GossipMenuAddItem(0, v[1], 0, i)
-			end
-		end
-		player:GossipSendMenu(1, unit)
-	else
-		print("This teleporter only supports 10 different menus.")
-	end
+local function OnGossipHello(event, player, unit)
+    -- Show main menu
+    for i, v in ipairs(T) do
+        if (v[2] == 2 or v[2] == player:GetTeam()) then
+            player:GossipMenuAddItem(0, v[1], i, 0)
+        end
+    end
+    player:GossipSendMenu(1, unit)
 end	
 
-local function Teleporter_Event(event, player, unit, sender, intid, code)
-	if(intid == 0) then
-		Teleporter_Gossip(event, player, unit)
-	elseif(intid <= 10) then
-		for i, v in ipairs(T[intid]) do
-			if (i > 2) then
-				player:GossipMenuAddItem(0, v[1], 0, intid..i)
-			end
-		end
-		player:GossipMenuAddItem(0, "Back", 0, 0)
-		player:GossipSendMenu(1, unit)
-	elseif(intid > 10) then
-		for i = 1, #T do
-			for j, v in ipairs(T[i]) do
-				if(intid == tonumber(i..j)) then
-					player:GossipComplete()
-					player:Teleport(v[2], v[3], v[4], v[5], v[6])
-				end
-			end
-		end
-	end
+local function OnGossipSelect(event, player, unit, sender, intid, code)
+    if (sender == 0) then
+        -- return to main menu
+        OnGossipHello(event, player, unit)
+        return
+    end
+
+    if (intid == 0) then
+        -- Show teleport menu
+        for i, v in ipairs(T[sender]) do
+            if (i > 2) then
+                player:GossipMenuAddItem(0, v[1], sender, i)
+            end
+        end
+        player:GossipMenuAddItem(0, "Back", 0, 0)
+        player:GossipSendMenu(1, unit)
+        return
+    else
+        -- teleport
+        local name, map, x, y, z, o = table.unpack(T[sender][action])
+        player:Teleport(map, x, y, z, o)
+    end
+    
+    player:GossipComplete()
 end
 
-RegisterCreatureGossipEvent(UnitEntry, 1, Teleporter_Gossip)
-RegisterCreatureGossipEvent(UnitEntry, 2, Teleporter_Event)
+RegisterCreatureGossipEvent(UnitEntry, 1, OnGossipHello)
+RegisterCreatureGossipEvent(UnitEntry, 2, OnGossipSelect)
