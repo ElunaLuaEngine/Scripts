76e64bb660ab4bbf3306f0b19f395a7439c59a63
CyberMist2 <cybermist2@gmail.com>
2020-04-25 01:11:16 +0300

    Regarding On Craft Skill Change New Player Hook PR
    Regarding https://github.com/ElunaLuaEngine/ElunaTrinityWotlk/pull/10

---
+++ Example Scripts/example_player_craft_skill_gain.lua
@@ -0,0 +1,6 @@
+local function OnCraftSkillChange(event, player, spellid, skillAmount)
+	player:SendBroadcastMessage("Congratulations you received +1 craft skill.")
+    return skillAmount + 1
+end
+
+RegisterPlayerEvent(40, OnCraftSkillChange)
